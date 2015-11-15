1c464e4ce1603e4895f0be7f4c02752cc7ab3d24
aimi19930326 <894249399@qq.com>
2015-11-15 10:48:21 +0800

    navandfoot

--- artfun/nav.Master.cs
+++ artfun/nav.Master.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace artfun.artwork
+namespace artfun
 {
-    public partial class work : System.Web.UI.Page
+    public partial class head : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
